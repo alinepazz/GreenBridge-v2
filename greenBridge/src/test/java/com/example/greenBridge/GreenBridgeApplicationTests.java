06d52a2950144a3b847352ff34f0b76224ee0e52
Aline Paz <aline.soares.2@hotmail.com>
2022-11-12 13:38:06 -0300

    criando projeto

---
+++ greenBridge/src/test/java/com/example/greenBridge/GreenBridgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.greenBridge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenBridgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
